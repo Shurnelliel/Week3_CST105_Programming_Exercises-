9594b012bd14a1a36c459fb19d1c01c8f9f74bb9
Shurnelliel <33744041+Shurnelliel@users.noreply.github.com>
2017-12-03 22:31:41 -0700

    Add files via upload

---
+++ Java_Exercise_3.java
@@ -0,0 +1,24 @@
+/** Program: Guessing Game Program
+* File: Java_Exercise.java
+* Summary: prompt the user to enter a guess.
+* Author: Shertonia S. Stanley
+* Date: December 3, 2017 
+**/
+ 
+//prompt the user to enter a guess
+          System.out.println(Guessing Game"); 
+     }
+}  
+
+
+
+Enter a guess between 1 and 10000: [7000]
+LOWER
+Enter a guess between 1 and 4999: [3500]
+LOWER
+Enter a guess between 1501 and 4999: [1750]
+You WIN. It took you three guesses.
+		
+public class Java_Exercise_3 {
+
+}
---
+++ Pig_latin.java
@@ -0,0 +1,23 @@
+/** Program: Reads text from a file Program
+* File: JPig_latin.java
+* Summary: write each program output word in Pig-latin .
+* Author: Shertonia S. Stanley
+* Date: December 3, 2017 
+**/
+ 
+//prompt the user to write each program out word in Pig-latin
+          System.out.println(Reads text from file"); 
+     }
+}  
+
+It was a cold morning in Arizona
+It ITYAY
+was ASWAY
+a AYAY
+cold OLDCAY
+morning ORNINGMAY
+in INYAY
+Arizona ARIZONAYAY
+public class Pig_latin {
+
+}
